b566eec18d113ac7b0fb871d7e28f0ea39b61163
James Belda <james.belda2014@gmail.com>
2016-06-20 20:04:53 +0800

    Added testimonials section

--- wp-content/themes/gloverzfotos/index.php
+++ wp-content/themes/gloverzfotos/index.php
@@ -2,8 +2,8 @@
 
 <!-- categories -->
 <div class="container content">
-	<h1>What we serve</h1>
-	<p class="subtitle">Here are the categories of services we provide</p>
+	<h1 class="centered">What we serve</h1>
+	<p class="subtitle centered">Here are the categories of services we provide</p>
 
 	<!-- cards -->
 	<div class="row categories">
@@ -47,7 +47,7 @@
 <div class="parallax-window" data-parallax="scroll" data-image-src="<?php bloginfo('template_directory'); ?>/resources/images/sample.jpg">
 	<div class="container content centered">
 		<h1>Demo Reel</h1>
-		<p class="subtitle">How we love to share to you our earlier works and commitments</p>
+		<p class="subtitle white">How we love to share to you our earlier works and commitments</p>
 		<iframe src="https://player.vimeo.com/video/167373612" class="demoreel" width="720" height="460" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
 	</div>
 </div>
@@ -58,6 +58,7 @@
 	<div class="details">
 		<div class="content-header centered">  
 			<h1>About our team</h1>
+			<hr>
 		</div>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a vulputate justo. Proin pulvinar urna nec vulputate eleifend. Nulla facilisi. Ut euismod in nisl quis fermentum. Donec pretium massa eget sapien fringilla mollis. In at auctor nibh, id sollicitudin arcu. Vivamus sem felis, aliquam vitae magna a, suscipit aliquet massa.</p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a vulputate justo. Proin pulvinar urna nec vulputate eleifend. Nulla facilisi. Ut euismod in nisl quis fermentum. Donec pretium massa eget sapien fringilla mollis. In at auctor nibh, id sollicitudin arcu. Vivamus sem felis, aliquam vitae magna a, suscipit aliquet massa.</p>
@@ -65,31 +66,101 @@
 	<div class="details">
 		<div class="content-header centered">  
 			<h1>Why trust us?</h1>
+			<hr>
 		</div>
 		<div class="row categories centered">
 			<div class="col-lg-3 col-sm-6 transp">
 				<div class="icon round img-circle affordable">&nbsp;</div>
 				<div class="title">Affordable</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit donec faucibus nulla ulcorper leo posue resodales.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
 			<div class="col-lg-3 col-sm-6 transp">
 				<div class="icon round img-circle fast-delivery">&nbsp;</div>
 				<div class="title">Fast Delivery</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit donec faucibus nulla ulcorper leo posue resodales.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
 			<div class="col-lg-3 col-sm-6 transp">
 				<div class="icon round img-circle creative">&nbsp;</div>
 				<div class="title">Creative</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit donec faucibus nulla ulcorper leo posue resodales.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
 			<div class="col-lg-3 col-sm-6 transp">
 				<div class="icon round img-circle quality-equipments">&nbsp;</div>
 				<div class="title">Quality Equipments</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit donec faucibus nulla ulcorper leo posue resodales.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
 		</div>
 	</div>
 </div>
 <!-- /about us -->
+
+<!-- contact us -->
+<div class="parallax-window contacts" data-parallax="scroll" data-image-src="<?php bloginfo('template_directory'); ?>/resources/images/contacts.jpg">
+	<div class="container content centered contacts">
+		<div class="details">
+			<h1>Give us a Call</h1>
+			<div class="info mobile">0926 893 8877</div>
+		</div>
+		<div class="details">
+			<span class="between">OR</span>
+		</div>
+		<div class="details">
+			<h1>Shoot us an email</h1>
+			<div class="info email">contact@gloverzfotos.com</div>
+		</div>
+	</div>
+</div>
+<!-- /contact us -->
+
+<!-- testimonials -->
+<div class="container content centered">
+	<h1>Testimonials</h1>
+	<p class="subtitle">What our clients say about us</p>
+	<!-- cards -->
+	<div class="row categories testimonials">
+		<div class="col-lg-3 col-sm-6 col-xs-12">
+			<div class="card testimonial">
+				<div class="icon people img-circle client-1">&nbsp;</div>
+				<div class="name">Sophia Valdez</div>
+				<div class="position">Wedding Coordinator</div>
+				<div class="message">
+					<p>Aenean imperdiet, orci sed mollis tempor, dolor est condimentum ex, sed mollis lectus erat quis tortor. Aenean dignissim nulla vitae.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-3 col-sm-6 col-xs-12">
+			<div class="card testimonial">
+				<div class="icon people img-circle client-2">&nbsp;</div>
+				<div class="name">James Belda</div>
+				<div class="position">Groom</div>
+				<div class="message">
+					<p>Aenean imperdiet, orci sed mollis tempor, dolor est condimentum ex, sed mollis lectus erat quis tortor. Aenean dignissim nulla vitae.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-3 col-sm-6 col-xs-12">
+			<div class="card testimonial">
+				<div class="icon people img-circle client-3">&nbsp;</div>
+				<div class="name">Roberto Diaz</div>
+				<div class="position">Marketing Manager</div>
+				<div class="message">
+					<p>Aenean imperdiet, orci sed mollis tempor, dolor est condimentum ex, sed mollis lectus erat quis tortor. Aenean dignissim nulla vitae.</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-3 col-sm-6 col-xs-12">
+			<div class="card testimonial">
+				<div class="icon people img-circle client-4">&nbsp;</div>
+				<div class="name">Judy Hopps</div>
+				<div class="position">Debutant</div>
+				<div class="message">
+					<p>Aenean imperdiet, orci sed mollis tempor, dolor est condimentum ex, sed mollis lectus erat quis tortor. Aenean dignissim nulla vitae.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end of cards -->
+</div>
+<!-- /testimonials -->
 
 <?php get_footer(); ?>
